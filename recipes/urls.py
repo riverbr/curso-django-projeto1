513a0ff0fbab9461bd8bf7b0a8e55d043dc37ff8
riverbr <bruno.river@live.com>
2023-01-04 11:12:14 -0300

    Uma nova URL para uma nova View

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from recipes.views import home
+from . import views
 
 
 urlpatterns = [
-    path('', home),  # Home
+    path('', views.home),  # Home
+    path('recipes/<int:id>/', views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -8,3 +8,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Bruno Santos',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Bruno Santos',
+    })
