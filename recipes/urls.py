6feea4a249a8494e251d7f49208010cd361ab7de
riverbr <bruno.river@live.com>
2023-01-09 16:05:29 -0300

    Corrigindo autor nulo

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,8 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home
-    path('recipes/category/<int:category_id>/', views.category, name='category'),
+    path(
+        'recipes/category/<int:category_id>/', views.category, name='category'
+    ),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]
