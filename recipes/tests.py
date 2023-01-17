e226392eb39d8115f868c7c1b35fbf67ba966989
riverbr <bruno.river@live.com>
2023-01-17 15:37:00 -0300

    Asserções (afirmações), Django Reverse URL e Pytest-Watch

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class RecipesURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        variavel = '123456'
-        print('OLÁ MUNDO')
-        assert 1 == 1, 'Um é igual a um'
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
