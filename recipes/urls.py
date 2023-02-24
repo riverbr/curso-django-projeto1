4f922d7491ff9df26e65eee6b82dd4b306e096e4
riverbr <bruno.river@live.com>
2023-02-24 16:41:33 -0300

    Criando a search view function com TDD (Test Driven Development)

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,9 +6,9 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home
+    path('recipes/search/', views.search, name='search'),
     path(
         'recipes/category/<int:category_id>/', views.category, name='category'
     ),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
-    path('recipes/search/', lambda request: ..., name='search'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -33,3 +33,7 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True,
     })
+
+
+def search(request):
+    ...
